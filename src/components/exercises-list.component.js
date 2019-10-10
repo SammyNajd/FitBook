ea0ef99781afe0f6b0a6dfe22cf90f8a104d9014
SammyNajd <snajd@ucsd.edu>
2019-10-10 15:23:32 -0700

    Fixed exercises-list component to include weight instead of username

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const Exercise = props => (
   <tr>
-    <td>{props.exercise.username}</td>
+    <td>{props.exercise.weight}</td>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
@@ -55,7 +55,7 @@ export default class ExercisesList extends Component {
         <table className="table">
           <thead className="thead-light">
             <tr>
-              <th>Username</th>
+              <th>Weight</th>
               <th>Description</th>
               <th>Duration</th>
               <th>Date</th>
