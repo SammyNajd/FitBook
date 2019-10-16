f9be1b7ddfc1de56cefb6d81ebb37b10cc2755a3
Max Shen <mshen.ivl@gmail.com>
2019-10-16 01:47:50 -0700

    Added more edit fields, fixed editing

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -7,19 +7,24 @@ export default class EditExercise extends Component {
   constructor(props) {
     super(props);
 
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
+      weight: 0, 
+      typeOfExercise: 'Cardio/PlyoMetrics',
       username: '',
       description: '',
       duration: 0,
       date: new Date(),
       users: []
     }
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeWeight = this.onChangeWeight.bind(this);
+    this.onChangeTypeOfExercise = this.onChangeTypeOfExercise.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    console.log(this.weight + "" + this.duration);
   }
 
   componentDidMount() {
@@ -27,7 +32,9 @@ export default class EditExercise extends Component {
       .then(response => {
         this.setState({
           username: response.data.username,
+          weight: response.data.weight,
           description: response.data.description,
+          typeOfExercise: response.data.typeOfExercise,
           duration: response.data.duration,
           date: new Date(response.data.date)
         })   
@@ -56,6 +63,17 @@ export default class EditExercise extends Component {
     })
   }
 
+  onChangeWeight(e) {
+    this.setState({
+      weight: e.target.value
+    })
+  }
+  onChangeTypeOfExercise(e) {
+    this.setState({
+      typeOfExercise: e.target.value
+    })
+  }
+
   onChangeDescription(e) {
     this.setState({
       description: e.target.value
@@ -78,6 +96,8 @@ export default class EditExercise extends Component {
     e.preventDefault();
 
     const exercise = {
+      weight: this.state.weight,
+      typeOfExercise: this.state.typeOfExercise,
       username: this.state.username,
       description: this.state.description,
       duration: this.state.duration,
@@ -114,13 +134,35 @@ export default class EditExercise extends Component {
               }
           </select>
         </div>
+        <div className="form-group">
+          <label>Type of Exercise</label>
+              <select ref="userInput"
+                required
+                className="form-control"
+                value={this.state.typeOfExercise}
+                onChange={this.onChangeTypeOfExercise}
+                >
+                  <option>Cardio/PlyoMetrics</option>
+                  <option>Weight/Resistance</option>
+                </select>
+        </div>
         <div className="form-group"> 
           <label>Description: </label>
-          <input  type="text"
+          <input  
+              type="text"
               required
               className="form-control"
               value={this.state.description}
               onChange={this.onChangeDescription}
+              />
+        </div>
+        <div className="form-group"> 
+          <label>Weight (lbs): </label>
+          <input  
+              type="text"
+              className="form-control"
+              value={this.state.weight}
+              onChange={this.onChangeWeight}
               />
         </div>
         <div className="form-group">
