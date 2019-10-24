2a2d376ce4ecd02189f95055f50ecd6d935a8b59
SammyNajd <snajd@ucsd.edu>
2019-10-23 17:19:01 -0700

    Cannot figure out a way to get the sorting to work. Check exercises-list for what I've done to see if anyone can get it to work.

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,185 +1,196 @@
-import React, {Component} from 'react';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css"; 
-import axios from 'axios';
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
-export default class CreateExercise extends Component{
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    // bind the keyword "this" change
+    this.onChangeWeight = this.onChangeWeight.bind(this);
+    this.onChangeTypeOfExercise = this.onChangeTypeOfExercise.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.postExercise = this.postExercise.bind(this);
+    this.onSubmitCreateNewExercise = this.onSubmitCreateNewExercise.bind(this);
 
-        // bind the keyword "this" change
-        this.onChangeWeight = this.onChangeWeight.bind(this);
-        this.onChangeTypeOfExercise = this.onChangeTypeOfExercise.bind(this);
-        this.onChangeDescription = this.onChangeDescription.bind(this);
-        this.onChangeDuration = this.onChangeDuration.bind(this);
-        this.onChangeDate = this.onChangeDate.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.postExercise = this.postExercise.bind(this);
-        this.onSubmitCreateNewExercise = this.onSubmitCreateNewExercise.bind(this);
+    // State is a variable in REACT
+    // ex you do not use let var =...
+    this.state = {
+      weight: 0,
+      typeOfExercise: "Cardio/PlyoMetrics",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+      redir: true
+    };
+  }
 
-        // State is a variable in REACT
-        // ex you do not use let var =...
-        this.state = {
-            weight: 0,
-            typeOfExercise: 'Cardio/PlyoMetrics',
-            description:'',
-            duration: 0,
-            date: new Date(),
-            users: [],
-            redir: true
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/users/")
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          });
         }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onChangeWeight(e) {
+    const reg = /^[0-9\b]+$/;
+    if (e.target.value === "" || reg.test(e.target.value)) {
+      this.setState({
+        weight: Number(e.target.value)
+      });
     }
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/users/')
-          .then(response => {
-            if (response.data.length > 0) {
-              this.setState({
-                users: response.data.map(user => user.username),
-                username: response.data[0].username
-              })
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-    
-      }
+  onChangeTypeOfExercise(e) {
+    this.setState({
+      typeOfExercise: e.target.value
+    });
+  }
 
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
 
-    onChangeWeight(e){
-        const reg = /^[0-9\b]+$/;
-        if(e.target.value === '' || reg.test(e.target.value)){
-            this.setState({
-                weight: Number(e.target.value)
-            })
-        }
+  onChangeDuration(e) {
+    const reg = /^[0-9\b]+$/;
+    if (e.target.value === "" || reg.test(e.target.value)) {
+      this.setState({
+        duration: Number(e.target.value)
+      });
     }
+  }
 
-    onChangeTypeOfExercise(e){
-        this.setState({
-            typeOfExercise: e.target.value
-        })
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  postExercise() {
+    const exercise = {
+      weight: this.state.weight,
+      typeOfExercise: this.state.typeOfExercise,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+    console.log(exercise);
+
+    axios
+      .post("http://localhost:5000/exercises/add", exercise)
+      .then(res => console.log(res.data));
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.postExercise();
+    this.redirectWindow(true);
+  }
+
+  onSubmitCreateNewExercise(e) {
+    e.preventDefault();
+    this.postExercise();
+    this.redirectWindow(false);
+  }
+
+  redirectWindow(red) {
+    if (red) {
+      window.location = "/";
+    } else {
+      window.location.reload();
     }
+  }
 
-    onChangeDescription(e){
-        this.setState({
-            description: e.target.value
-        })
-    }
-
-    onChangeDuration(e){
-        const reg = /^[0-9\b]+$/;
-        if(e.target.value === '' || reg.test(e.target.value)){
-            this.setState({
-                duration: Number(e.target.value)
-            })
-        }
-    }
-
-    onChangeDate(date){
-        this.setState({
-            date: date
-        })
-    }
-
-    postExercise(){
-        const exercise = {
-            weight: this.state.weight,
-            typeOfExercise: this.state.typeOfExercise,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date
-        }
-        console.log(exercise);
-
-        axios.post('http://localhost:5000/exercises/add', exercise)
-            .then(res => console.log(res.data));
-    }
-
-    onSubmit(e){
-        e.preventDefault();
-        this.postExercise();
-        this.redirectWindow(true);
-    }
-
-    onSubmitCreateNewExercise(e){
-        e.preventDefault();
-        this.postExercise();
-        this.redirectWindow(false);
-    }
-
-    redirectWindow(red){
-        if(red){
-            window.location = '/'
-        }
-        else{
-            window.location.reload();
-        }
-    }
-
-    //left off at 11134
-    render(){
-        return(
+  //left off at 11134
+  render() {
+    return (
+      <div>
+        <h3> Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Type of Exercise</label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.typeOfExercise}
+              onChange={this.onChangeTypeOfExercise}
+            >
+              <option>Cardio/PlyoMetrics</option>
+              <option>Weight/Resistance</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Weight (lbs): </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.weight}
+              onChange={this.onChangeWeight}
+            />
+          </div>
+          <div className="form-group">
+            <label> Workout Name: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label> Duration (reps/minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
             <div>
-                <h3> Create New Exercise Log</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Type of Exercise</label>
-                        <select ref="userInput"
-                            required
-                            className="form-control"
-                            value={this.state.typeOfExercise}
-                            onChange={this.onChangeTypeOfExercise}
-                            >
-                                <option>Cardio/PlyoMetrics</option>
-                                <option>Weight/Resistance</option>
-                            </select>
-                    </div>
-                    <div className="form-group">
-                        <label>Weight (lbs): </label>
-                        <input type="text"
-                               required
-                               className="form-control"
-                               value={this.state.weight} 
-                               onChange={this.onChangeWeight} 
-                               />
-                    </div>
-                    <div className="form-group">
-                        <label> Workout Name: </label>
-                        <input type="text"
-                            required
-                            className="form-control"
-                            value={this.state.description}
-                            onChange={this.onChangeDescription}
-                            />
-                    </div>
-                    <div className="form-group">
-                        <label> Duration (reps/minutes): </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={this.state.duration}
-                            onChange={this.onChangeDuration}
-                            />
-                    </div>
-                    <div className="form-group">
-                        <label>Date: </label>
-                        <div>
-                            <DatePicker
-                                selected={this.state.date}
-                                onChange={this.onChangeDate}
-                                />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <input type="submit" onClick={this.onSubmit} value="Create Exercise and View Log" className="btn btn-primary" />
-                    </div>
-                    <input type="submit" onClick={this.onSubmitCreateNewExercise} value="Create Another Exercise" className="btn btn-primary" />
-                </form>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
             </div>
-        )
-    }
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              onClick={this.onSubmit}
+              value="Create Exercise and View Log"
+              className="btn btn-primary"
+            />
+          </div>
+          <input
+            type="submit"
+            onClick={this.onSubmitCreateNewExercise}
+            value="Create Another Exercise"
+            className="btn btn-primary"
+          />
+        </form>
+      </div>
+    );
+  }
 }
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import {DropdownButton, Dropdown} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const Exercise = props => (
   <tr>
@@ -9,108 +9,151 @@ const Exercise = props => (
     <td>{props.exercise.typeOfExercise}</td>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
     <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
     </td>
   </tr>
-)
+);
 
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
 
-    this.deleteExercise = this.deleteExercise.bind(this)
-    this.onChangeSearch = this.onChangeSearch.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.onSelectDropDown = this.onSelectDropDown.bind(this)
+    this.deleteExercise = this.deleteExercise.bind(this);
+    this.onChangeSearch = this.onChangeSearch.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.onSelectDropDown = this.onSelectDropDown.bind(this);
+    this.sortFilteredExercises = this.sortFilteredExercises.bind(this);
+    this.sortExercises = this.sortExercises.bind(this);
 
-    this.state = {exercises: [],
-                  searchRes: "",
-                  dropDownSel: "Type",
-                  filteredExercises: []};
+    this.state = {
+      exercises: [],
+      searchRes: "",
+      dropDownSel: "Type",
+      filteredExercises: []
+    };
   }
 
-
-
-  onChangeSearch(e){
+  onChangeSearch(e) {
     const search = e.target.value.toLowerCase();
 
-    if(this.state.dropDownSel === "Description"){
+    if (this.state.dropDownSel === "Description") {
       this.setState({
         searchRes: search,
-        filteredExercises: this.state.exercises.filter(
-          (exer) => exer.description.toLowerCase().includes(search)
+        filteredExercises: this.state.exercises.filter(exer =>
+          exer.description.toLowerCase().includes(search)
+        )
+      });
+    } else if (this.state.dropDownSel === "Type") {
+      this.setState({
+        searchRes: search,
+        filteredExercises: this.state.exercises.filter(exer =>
+          exer.typeOfExercise.toLowerCase().includes(search)
+        )
+      });
+    } else {
+      this.setState({
+        searchRes: search,
+        filteredExercises: this.state.exercises.filter(exer =>
+          exer.date.includes(search)
         )
       });
     }
-
-    else if(this.state.dropDownSel === "Type"){
-      this.setState({
-        searchRes: search,
-        filteredExercises: this.state.exercises.filter(
-          (exer) => exer.typeOfExercise.toLowerCase().includes(search)
-        )
-      });
-    }
-
-    else{
-      this.setState({
-        searchRes: search,
-        filteredExercises: this.state.exercises.filter(
-          (exer) => exer.date.includes(search)
-        )
-      });
-    }
+    //this.sortFilteredExercises();
   }
 
+  sortFilteredExercises() {
+    this.setState({
+      filteredExercises: this.state.filteredExercises.sort(
+        (date1, date2) => date2.date - date1.date
+      )
+    });
+    console.log("We sorting bruh");
+    console.log(this.state.filteredExercises);
+  }
 
+  onSelectDropDown(evt) {
+    this.setState({ dropDownSel: evt });
+  }
 
-  onSelectDropDown(evt){
-    this.setState({dropDownSel: evt});
+  sortExercises() {
+    console.log("sorting");
+    this.setState({
+      exercises: this.state.exercises.sort(function(date1, date2) {
+        console.log("Date1: " + date1.date);
+        console.log("Date2: " + date2.date);
+        return date1.date - date2.date;
+      })
+    });
+    console.log(this.state.exercises);
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/')
+    axios
+      .get("http://localhost:5000/exercises/")
       .then(response => {
-        this.setState({ exercises: response.data, 
-                        filteredExercises: response.data})
+        this.setState({
+          exercises: response.data,
+          filteredExercises: response.data
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
-      })
+      });
+    console.log("exercises: " + this.state.exercises);
+    this.sortExercises();
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
-      .then(response => { console.log(response.data)});
+    axios.delete("http://localhost:5000/exercises/" + id).then(response => {
+      console.log(response.data);
+    });
     this.setState({
       exercises: this.state.exercises.filter(el => el._id !== id),
       filteredExercises: this.state.exercises.filter(el => el._id !== id)
-    })
+    });
   }
 
   exerciseList() {
     return this.state.filteredExercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
-    })
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
   }
-
 
   render() {
     return (
       <div>
         <h3>Logged Exercises</h3>
-        <DropdownButton id="dropdown-basic-button" title={"Filter by " + this.state.dropDownSel} onSelect={this.onSelectDropDown}> 
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={"Filter by " + this.state.dropDownSel}
+          onSelect={this.onSelectDropDown}
+        >
           <Dropdown.Item eventKey="Type">Type of Exercise</Dropdown.Item>
           <Dropdown.Item eventKey="Description">Description</Dropdown.Item>
-          <Dropdown.Item eventKey="Date">Date</Dropdown.Item> 
+          <Dropdown.Item eventKey="Date">Date</Dropdown.Item>
         </DropdownButton>
-        <input type="text"
-                className="form-control"
-                value={this.state.searchRes}
-                onChange={this.onChangeSearch}
-                />
+        <input
+          type="text"
+          className="form-control"
+          value={this.state.searchRes}
+          onChange={this.onChangeSearch}
+        />
 
         <table className="table">
           <thead className="thead-light">
@@ -123,11 +166,9 @@ export default class ExercisesList extends Component {
               <th>Actions</th>
             </tr>
           </thead>
-          <tbody>
-            { this.exerciseList() }
-          </tbody>
+          <tbody>{this.exerciseList()}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
