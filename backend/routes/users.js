8962bb89b40b311c56aeb00320049904aad89e0a
Max Shen <mshen.ivl@gmail.com>
2019-10-30 12:58:04 -0700

    testing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,24 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const newUser = new User({username, password});
+  const newUser = new User({ username, password });
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
 
-
+//test
